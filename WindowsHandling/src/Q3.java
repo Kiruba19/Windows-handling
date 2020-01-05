8268ebb5e2143a4a4d8fba444290056d5fc3366c
Kiruba19 <kiruprettygood@gmail.com>
2020-01-05 07:22:16 +0530

    windows handling

--- WindowsHandling/src/Q3.java
+++ WindowsHandling/src/Q3.java
@@ -16,23 +16,32 @@ public class Q3 {
 		WebElement search = driver.findElement(By.id("inputValEnter"));
 		search.sendKeys("iphone7", Keys.ENTER);
 		Thread.sleep(5000);
-		driver.findElement(By.xpath("(//p[text()='Apple iPhone 7 ( 32GB , 2 GB ) Black'])[1]")).click();
-		String parentw = driver.getWindowHandle();
-		System.out.println(parentw);
-		Set<String> allwindow = driver.getWindowHandles();
-		System.out.println(allwindow);
-		for (String childwindow : allwindow) {
-			
-			if(!parentw.equals(childwindow)) {
-				driver.switchTo().window(childwindow);
+		driver.findElement(By.xpath("//p[text()='iPhone 7 Soft Silicon Cases O4U - Pink Plain Back Cover']")).click();
+		
+		
+	//parent window id
+		
+		String parent = driver.getWindowHandle();
+		
+		System.out.println(parent);
+		
+		//all windows id
+		
+		Set<String> all = driver.getWindowHandles();
+		
+		System.out.println(all);
+		//iterate set using enhanced for loop
+		
+		for(String x:all) {
+		
+			//Chcking arent is not equals to child , if it not equals then switch
+			if(!parent.equals(x)) {  //12   12  //12 1213
+				driver.switchTo().window(x);
+				
 			}
-			System.out.println("switched to child window");
 		}
-	driver.findElement(By.id("add-cart-button-id")).click();
-	Thread.sleep(3000);
-	WebElement text = driver.findElement(By.xpath("(//span[text()='Rs. 30,651'])[2]"));
-	System.out.println(text.getText());
 
-	}
-
+		driver.findElement(By.xpath("//span[text()='add to cart']")).click();
+		
+}
 }
